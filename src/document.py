5b70748869f14666b4bc6a67c679bde8d737d081
Gabriel Majivu <gabriel@angaza.com>
2022-02-28 23:15:57 +0300

    some formatting

--- src/document.py
+++ src/document.py
@@ -10,25 +10,33 @@ class Actions(str, Enum):
 
 ID = "_id"
 SUB_DOCS = ["mentions", "posts"]
-FIELDS = ["value", "text"]
 
 
-def find_index(document, path, doc):
+def find_index(
+    document: dict[str, Any], path: list[str], doc: dict[str, Any]
+) -> int:
+    """Find a document position give its current path"""
     if len(path) == 0:
         for idx, item in enumerate(document):
             if ID not in doc:
                 return -1
-            if doc["_id"] == item["_id"]:
+            if doc[ID] == item[ID]:
                 return idx
     _p = int(path[0]) if path[0].isnumeric() else path[0]
     return find_index(document[_p], path[1:], doc)
 
 
-def search(document, node, path):
+def search(
+    document: dict[str, Any], node: dict[str, Any], path: list[str]
+) -> list[str]:
+    """Search document recursively to determine action"""
     if ID in node:
         idx = find_index(document, path, node)
         path.append(str(idx))
-    if isinstance(node, dict) and ("value" in node or "text" in node or "_delete" in node):
+
+    if isinstance(node, dict) and (
+        "value" in node or "text" in node or "_delete" in node
+    ):
         if "value" in node:
             if ID in node:
                 path.append(f"{Actions.UPDATE}:value:{node['value']}")
@@ -42,6 +50,7 @@ def search(document, node, path):
         if "_delete" in node:
             path.append(f"{Actions.REMOVE}::true")
         return path
+
     for k, v in node.items():
         if k in SUB_DOCS:
             path.append(k)
@@ -50,7 +59,10 @@ def search(document, node, path):
     return path
 
 
-def generate_update_statement(document: dict[str, Any], input: dict[str, Any]):
+def generate_update_statement(
+    document: dict[str, Any], input: dict[str, Any]
+) -> dict[str, dict[str, str]]:
+    """generate update statement for given edit actions"""
     result = {}
 
     for k, cmds in input.items():
